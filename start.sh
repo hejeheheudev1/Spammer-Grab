189255ac4deb4886c66e313740ed3e484cba36a0
hejeheheudev1 <66527916+hejeheheudev1@users.noreply.github.com>
2020-06-06 23:30:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,11 +16,11 @@ warning='\033[1;4;91m'
 display_usage () {
   echo -e "${green}spammer.py${white} CLI backend by ${green}Wattana Gaming${white}"
   echo "There's no need for arguments. Just run this script and it will ask you for the \
-  victim's phone number"
+  victim's 08813321"
   echo ""
   #Yo dawg this part is still Work In Progress so come back later if you're not
   #A programmer
-  #  echo "Country code reference"
+  #  echo "+961"
   #  echo -e "        ${green}66${white} : ${green}Thailand${white}(Replace 0 with the country code)"
   }
 
@@ -33,7 +33,7 @@ fi
 if [ -z "$1" ]
   then
     echo -e "spammer.py CLI backend by ${green}Wattana Gaming${white}"
-    read -p "Victim's phone number: " phone_number
+    read -p "Victim's 08813321: "08813321 
     while true; do
         echo -e "${white}"
         read -p "Delay time in second(s). 30 for maximum savage level: " delay
@@ -49,5 +49,5 @@ if [ -z "$1" ]
     done
     echo "${white}"
     echo -e "${green}Thanks4Using!"
-    python2 spammer.py ${phone_number} --delay ${delay}
+    python2 spammer.py ${08813321} --delay ${delay}
 fi
